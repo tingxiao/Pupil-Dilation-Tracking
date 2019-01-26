cc43a5d7e93b2af5240386e9394b1e0411df93fd
Julia <jadamski@luc.edu>
2019-01-26 17:45:48 -0600

    option to upload already extracted frames added

--- upload.py
+++ upload.py
@@ -21,11 +21,9 @@ def openFile():
     options = QFileDialog.Options()
     options |= QFileDialog.DontUseNativeDialog
     fileName, _ = QFileDialog.getOpenFileName(None,"QFileDialog.getOpenFileName()", "","Video Files (* avi);;Python Files (*.py)", options=options)
-    if fileName:
-        print(fileName)
+#    if fileName:
+#        print(fileName)
     return fileName
-
-
 
 def splitVideo(vidFileName, imageListInput, folderName):
     global vidcap
@@ -40,10 +38,6 @@ def splitVideo(vidFileName, imageListInput, folderName):
             cv2.imwrite(os.path.join(folder,"frame{:d}.jpg".format(framecount)), image)     # save frame as JPEG file
             print("{} images are extacted in {}.".format(framecount,folder))
             framecount += 1
- 
-    if not os.path.exists(folder + "_FRAMEOUTPUT"):
-        os.mkdir(folder + "_FRAMEOUTPUT")
- 
         
 def getFPS():
     #note: This fuction does not work if the video does not have the FPS in its properties
@@ -51,36 +45,32 @@ def getFPS():
     fps = vidcap.get(cv2.CAP_PROP_FPS)
     return fps 
 
-def calculateFPS():
-    #Just for testing on specific vids
-    s = 41
-    frames = 737
-    fps = frames/s
-    return fps
+def openFrames():
+    fileName = openFile() # Filename of whichever frame is clicked
+    directory = os.path.split(fileName)[0] #Directory that the video frames are stored in
+    path, dirs, files = next(os.walk(directory))
+    file_count = len(files)
+    populateArray(file_count,directory)
 
-
+    
 def openVidFile():
     import ui_MAIN
-    from ui_MAIN import MyMainWindow
     global image_list
 
     fileName = openFile() #openFile() opens file browser and returns name of selected video file
     directory = str(QFileDialog.getExistingDirectory(ui_MAIN.MyMainWindow(),"Select Folder to Store Frames")) # File dialog opens for user to create/selet a folder to store the frames extracted from video
-    print("directory is:", directory)
     splitVideo(fileName, image_list, directory) 
-    
-
-    #TO DO: add loading status bar while frames are being uploaded      
-    
     path, dirs, files = next(os.walk(directory))
     file_count = len(files)
+    populateArray(file_count,directory)
 
+
+def populateArray(file_count, directory):
     for x in range(1, file_count + 1):
         image_list.append(directory + "/frame" + str(x) + ".jpg")
         
-    #ui_MAIN.MyMainWindow.horizontalSlider.setRange(0,len(image_list)-1)
-    #ui_MAIN.MyMainWindow.video_title.setText(fileName)
-    #ui_MAIN.MyMainWindow().add_img()
+    if not os.path.exists(directory + "_FRAMEOUTPUT"):
+        os.mkdir(directory + "_FRAMEOUTPUT")
 
 
 def img_list():
